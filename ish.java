50ac8dbf006f7251cc4455c394a7134a7104da50
21051999 <47914763+21051999@users.noreply.github.com>
2019-03-08 00:24:32 -0800

    Create ish.java

---
+++ ish.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a;
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		if(a%2==0)
+		{
+		System.out.println("even");
+		}
+		else if(a%2==0)
+		{
+		System.out.println("odd");
+		}		
+		else
+		{
+		System.out.println("invalid");
+		}
+}
+}
